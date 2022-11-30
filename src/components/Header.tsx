5eae02848c8c0cd70fbe6f60af08d88b4fe9325d
Marcos Pablo <rinpikibr@gmail.com>
2022-11-30 16:54:57 -0300

    fix: correçao sobre btns em telas mobile maiores

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ function Header() {
   return (
     <header className="flex-col items-center">
       <img src={logo} alt="xbox" className="my-[24px] mx-auto" />
-      <ul className="text-[9px] text-white flex space-x-5">
+      <ul className="text-[8px] mmb:text-[10px] text-white flex space-x-5">
         <li className="bg-[#67676763] py-[6px] px-[10px] rounded-2xl">
-          visão geral
+          VISÃO GERAL
         </li>
         <li className="bg-[#67676763] py-[6px] px-[10px] rounded-2xl">JOGOS</li>
         <li className="bg-[#67676763] py-[6px] px-[10px] rounded-2xl">
@@ -15,6 +15,9 @@ function Header() {
         </li>
         <li className="bg-[#67676763] py-[6px] px-[10px] rounded-2xl">UNBOX</li>
       </ul>
+      <div className="text-xs mbb:text-sm py-3 px-[70px] mmb:px-[90px] bg-[#9AF10C] mt-[56px] text-[#0C0C0C] font-bold text-center">
+        VER DISPONIBILIDADE
+      </div>
     </header>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mmb: '360px',
+        mbb: '375px',
+      },
+    },
   },
   plugins: [],
 };
