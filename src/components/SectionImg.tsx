4b03a6c4e5fec4744094adcf8f9bdc7242453709
Marcos Pablo <rinpikibr@gmail.com>
2022-12-07 19:47:40 -0300

    feat: imgs add em todas as telas

--- src/components/SectionImg.tsx
+++ src/components/SectionImg.tsx
@@ -6,22 +6,22 @@ import fourText from '../imgs/games/fourtext.png';
 
 function SectionImg() {
   return (
-    <div className="flex flex-col space-y-8 mb-28">
-      <div className="bg-one w-[312px] h-[497px]">
+    <div className="flex flex-col space-y-8 mb-28 md:flex-wrap md:flex-row md:justify-center md:space-y-0 ">
+      <div className="bg-one w-[312px] h-[497px] md:mr-10 md:mb-10 lg:mb-0 lg:mr-0 ">
         <img
           src={oneText}
           alt="titulo game farcry"
-          className="w-[190px] h-[42px] mx-auto mt-[414px]"
+          className="w-[190px] h-[42px] mx-auto mt-[414px] "
         />
       </div>
-      <div className="bg-two w-[312px] h-[493px]">
+      <div className="bg-two w-[312px] h-[493px] md:mb-10 lg:mb-0">
         <img
           src={twoText}
           alt="titulo game forza"
           className="w-[195px] h-[54px] mx-auto mt-[414px]"
         />
       </div>
-      <div className="bg-three w-[312px] h-[497px]">
+      <div className="bg-three w-[312px] h-[497px] md:mr-10 lg:mb-0 lg:mr-0">
         <img
           src={threeText}
           alt="titulo game fifa"
